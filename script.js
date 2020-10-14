172bfe959022a54085a9645aca5a809c3bfa80e5
Gabriel Seals <sealsgj@gmail.com>
2020-10-13 19:27:09 -0500

    stopping for the night

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizData = [
+  {
+    question: 'Hold old is Florin?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '110',
+    correct: 'c'
+  },
+  {
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd'
+  },
+  {
+    question: 'Who is the president of the US?',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    correct: 'b'
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'a'
+  }
+]
